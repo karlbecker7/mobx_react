a830fe567325535ce176c806ee3da7a1ff22e778
karlbecker7 <karlbecker@gmx.com>
2017-03-29 21:01:02 +0200

    Integrate initial filter

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,30 @@ import React, {Component} from 'react';
 import {observer} from 'mobx-react';
 
 class TodoList extends Component{
+
+	filter(e){
+		this.props.store.filter = e.target.value;
+	}
+
 	render(){
+
+		const {todos, filter, filteredTodos} = this.props.store;
+
+		const todoLis = filteredTodos.map( todo=>(
+				<li key={todo}>{todo}</li>
+			)			
+		);		
+
 		return (
-			<h1>Mobx {this.props.store.filter}</h1>
+			<div>
+				<h1>Todos</h1>
+
+				<input onChange={e=>this.filter(e)}/>
+				<div>{filter}</div>
+				<ul>
+					{todoLis}
+				</ul>
+			</div>
 		)
 	}
 }
--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,11 +1,17 @@
-import {extendObservable, autorun} from "mobx";
+import {extendObservable, autorun, computed} from "mobx";
 
 class TodoStore {
 
 	constructor(){
 		extendObservable(this, {
 				todos : ["buy milk", "buy eggs"],
-				filter : "m"
+				filter : "",
+				filteredTodos : computed(()=>{
+					var matchFilter = new RegExp(this.filter, "i");
+					return this.todos.filter(
+						todo=>{return matchFilter.test(todo);}
+					)
+				})
 			}
 		);
 	}
